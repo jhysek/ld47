48901a73222d953d6ec117205e65fa84d948e4eb
Jiri Hysek <jiri.hysek@gmail.com>
2020-10-02 22:21:29 +0200

    Empty game template

---
+++ Scenes/Light2D.gd
@@ -0,0 +1,14 @@
+extends Light2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	rotation += delta;	
