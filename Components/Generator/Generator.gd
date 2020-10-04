1ae2886bc0db3eb6baffa37591f2e1da3953b137
Jiri Hysek <jiri.hysek@gmail.com>
2020-10-04 19:24:21 +0200

    Level design... still

--- Components/Generator/Generator.gd
+++ Components/Generator/Generator.gd
@@ -2,14 +2,18 @@ extends Sprite
 
 onready var world = get_node("/root/World")
 
+var on = false;
+
 func power_on():
-	$Light2D.enabled = true
-	$TurnOn.play()
-	$HandleOff.hide()
-	$HandleOn.show()
-	$light.modulate = Color("7cb400")
-	$bolt.show()
-	world.generator_turned_on()
+	if !on:
+		on = true
+		$Light2D.enabled = true
+		$TurnOn.play()
+		$HandleOff.hide()
+		$HandleOn.show()
+		$light.modulate = Color("7cb400")
+		$bolt.show()
+		world.generator_turned_on()
 
 
 func _on_Area2D_body_entered(body):
